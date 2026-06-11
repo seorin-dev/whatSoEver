2a241598daf504d42aa706bc2875e0be7234ecc1
배동연 <baedong-yeon@baedong-yeon-ui-MacBookPro.local>
2026-06-11 12:49:58 +0900

    chore: Next.js + Vitest 스캐폴딩

---
+++ src/types/lunar-javascript.d.ts
@@ -0,0 +1,4 @@
+declare module 'lunar-javascript' {
+  export const Solar: any; // eslint-disable-line @typescript-eslint/no-explicit-any
+  export const Lunar: any; // eslint-disable-line @typescript-eslint/no-explicit-any
+}
